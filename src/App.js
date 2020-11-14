fedb01351cebc616056bf8a86aa84880ab196356
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-11-13 21:48:10 -0600

    data pulling correctly. just need to figure out styling and data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState, useEffect } from "react";
 import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Container from "./components/Container";
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [employees, showEmployees] = useState([]);
+  
   useEffect(() => {
     axios.get("https://randomuser.me/api/?results=5&nat=us")
-      .then(response => {
-        console.log(response)
+      .then((response) => {
+        console.log(response.data.results);
+        showEmployees([...response.data.results]);
       })
   }, [])
 
   return (
     <div className="App">
+      <Header/>
+      <Container employees = {employees}/>
     </div>
   );
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,31 +1,28 @@
-import React, { Component } from "react";
-import EmployeeList from "./EmployeeList";
-import API from "../utils/API";
+// import React, { useState, useEffect } from "react";
 
-class Container extends Component{
-    state = {
-        search: "",
-        results: []
-      };
-    
-    
-    generateList = query => {
-        API.search(query)
-          .then(res => this.setState({ results: res.data.data }))
-          .catch(err => console.log(err));
-      };
+const Container = (props) => {
+    return (
+        props.employees.map(employee => {
+            console.log(employee.name)
+           
+                <div class="card mb-3" style = "max-width: 540px;" >
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="..." class="card-img" alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        })
+    )
 
 
-    render(){
-        return(
-            <div>
-<EmployeeList>
-
-</EmployeeList>
-
-            </div>
-        )
-    }
 }
 
 export default Container;
