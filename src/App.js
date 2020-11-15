5e26d2227a5e914d4c006aafa4f290d3b12480e5
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-11-15 17:35:27 -0600

    working on search function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer, useRef } from "react";
 import Header from "./components/Header";
 import EmployeeContainer from "./components/Container";
+import Navbar from "./components/Navbar";
 
 // import logo from './logo.svg';
 import './App.css';
 
+// export default class DataArea extends Component{
+//  state = {
+//    employees: [{}],
+//   filteredEmployees: [{}]
+//  }
+
+//  searchFilter = event => {
+//   console.log(event.target.value);
+//   const filter = event.target.value;
+//   const filteredList = this.state.employees.filter(item => {
+//     // merge data together, then see if user input is anywhere inside
+//     let values = Object.values(item)
+//       .join("")
+//       .toLowerCase();
+//     return values.indexOf(filter.toLowerCase()) !== -1;
+//   });
+//   this.setState({ filteredEmployees: filteredList });
+// }
+
+//  componentDidMount() {
+//   axios.get("https://randomuser.me/api/?results=5&nat=us")
+//       .then(results => {
+//     this.setState({
+//       employees: results.data.results,
+//       filteredEmployees: results.data.results
+      
+//     },
+//     console.log(results.data.results)
+//     );
+//   });
+// }
+
+// render() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Navbar searchFilter={this.searchFilter}/>
+//       <EmployeeContainer employees={employees} />
+//     </div>
+//   );
+// }
+
+// }
+
 function App() {
-  const [employees, showEmployees] = useState([]);
-  
+  const [employees, showEmployees, filteredEmployees] = useState([]);
   useEffect(() => {
     axios.get("https://randomuser.me/api/?results=5&nat=us")
       .then((response) => {
         console.log(response.data.results);
         showEmployees([...response.data.results]);
+        filteredEmployees([...response.data.results]);
+
       });
   }, []);
 
+  searchFilter = event => {
+    console.log(event.target.value);
+    const filter = event.target.value;
+    const filteredList = this.state.users.filter(item => {
+      // merge data together, then see if user input is anywhere inside
+      let values = Object.values(item)
+        .join("")
+        .toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    this.setState({ filteredEmployees: filteredList });
+  }
+
   return (
     <div className="App">
-      <Header/>
-      <EmployeeContainer employees = {employees}/>
+      <Header />
+      <Navbar searchFilter={this.searchFilter}/>
+      <EmployeeContainer employees={employees} />
     </div>
   );
 }
