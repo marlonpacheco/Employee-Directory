2a864951116c14699541cda8c323e4e1b02fba4b
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-11-19 20:48:20 -0600

    fixed search box

--- src/App.js
+++ src/App.js
@@ -52,35 +52,38 @@ import './App.css';
 // }
 
 function App() {
-  const [employees, showEmployees, filteredEmployees] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
   useEffect(() => {
-    axios.get("https://randomuser.me/api/?results=5&nat=us")
+    axios.get("https://randomuser.me/api/?results=50&nat=us")
       .then((response) => {
         console.log(response.data.results);
-        showEmployees([...response.data.results]);
-        filteredEmployees([...response.data.results]);
+        setEmployees([...response.data.results]);
+        setFilteredEmployees([...response.data.results]);
 
       });
   }, []);
 
-  searchFilter = event => {
+
+  const searchFilter = event => {
     console.log(event.target.value);
     const filter = event.target.value;
-    const filteredList = this.state.users.filter(item => {
+    const filteredList = employees.filter(item => {
       // merge data together, then see if user input is anywhere inside
       let values = Object.values(item)
         .join("")
         .toLowerCase();
       return values.indexOf(filter.toLowerCase()) !== -1;
     });
-    this.setState({ filteredEmployees: filteredList });
+   setFilteredEmployees(filteredList)
+    console.log(filteredList)
   }
 
   return (
     <div className="App">
       <Header />
-      <Navbar searchFilter={this.searchFilter}/>
-      <EmployeeContainer employees={employees} />
+      <Navbar searchFilter={searchFilter}/>
+      <EmployeeContainer employees={filteredEmployees} />
     </div>
   );
 }
